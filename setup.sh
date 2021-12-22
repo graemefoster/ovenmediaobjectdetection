fe46a35b928f741bce4046c10ac81caa0790e89b
Graeme Foster <graemefoster@gmail.com>
2021-12-22 11:57:02 +0800

    Not in docker

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
-RUN pip3 install -r ./requirements.txt
-RUN pip3 install -qr https://raw.githubusercontent.com/ultralytics/yolov5/master/requirements.txt
+pip3 install -r ./requirements.txt
+pip3 install -qr https://raw.githubusercontent.com/ultralytics/yolov5/master/requirements.txt
